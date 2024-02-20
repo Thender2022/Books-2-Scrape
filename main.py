76c0ca622cafaae66432d58bd7728027e66e000d
Tyrone Henderson <Thender2022@gmail.com>
2024-02-19 21:47:36 -0800

    all scraped

--- main.py
+++ main.py
@@ -1,125 +1,275 @@
 import requests
 import csv
 from bs4 import BeautifulSoup
-
-# def scrape_product_page(url):
-#     response = requests.get(url)
-#
-#     if response.status_code == 200:
-#         soup = BeautifulSoup(response.text, 'html.parser')
-#
-#         #extracting single product
-#         product_page_url = url
-#         book_title = soup.h1.text.strip()
-#         product_description = soup.find('meta', {'name': 'description'})['content'].strip()
-#         upc_th = soup.find('th', string='UPC')
-#         upc = upc_th.find_next('td').text.strip()
-#         price_including_tax = soup.find('th', text='Price (incl. tax)').find_next('td').string.strip()
-#         price_excluding_tax = soup.find('th', text='Price (excl. tax)').find_next('td').text.strip()
-#         quantity_available = soup.find('th', text='Availability').find_next('td').text.strip()
-#         category = soup.find('a', {'href': '../category/books/business_35/index.html'}).text.strip()
-#         review_rating = soup.find('th', text='Number of reviews').find_next('td').text.strip()
-#         image_url = soup.find('div', {'class': 'item active'}).img['src']
-#
-#         # Writing single book info to csv
-#         csv_filename = 'book_data.csv'
-#         with open(csv_filename, 'w', newline='') as csvfile:
-#             fieldnames = ['product_page_url', 'upc', 'book_title', 'price_including_tax',
-#                           'price_excluding_tax', 'quantity_available', 'product_description',
-#                           'category', 'review_rating', 'image_url']
-#             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-#
-#             writer.writeheader()
-#
-#             writer.writerow({
-#                 'product_page_url': product_page_url,
-#                 'upc': upc,
-#                 'book_title': book_title,
-#                 'price_including_tax': price_including_tax,
-#                 'price_excluding_tax': price_excluding_tax,
-#                 'quantity_available': quantity_available,
-#                 'product_description': product_description,
-#                 'category': category,
-#                 'review_rating': review_rating,
-#                 'image_url': image_url
-#             })
-#
-#             print(f"Data written to {csv_filename}")
-#
-#         print(f"Product Page URL: {product_page_url}")
-#         print(f"Book Title: {book_title}")
-#         print(f"Product Description: {product_description}")
-#         print(f"UPC: {upc}")
-#         print(f"Price Including Tax: {price_including_tax}")
-#         print(f"Price Excluding Tax: {price_excluding_tax}")
-#         print(f"Quantity Available: {quantity_available}")
-#         print(f"Category: {category}")
-#         print(f"Review Rating: {review_rating}")
-#         print(f"Image URL: {image_url}")
+from urllib.parse import urljoin
 
 
-def scrape_category_page(category_url):
+def scrape_site(base_url):
+    response = requests.get(base_url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Find all the category links
+        category_links = soup.find_all('a', href=lambda href: href and 'catalogue/category/books' in href)
+
+        for category_link in category_links:
+            relative_url = category_link['href']
+            category_url = urljoin(base_url, relative_url)
+            print("Category URL:", category_url)
+
+            scrape_category_books(category_url)
+
+def scrape_category_books(category_url):
     response = requests.get(category_url)
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
 
-#     Extracting Category Page
-        category_page_url = category_url
-        book_containers = soup.find_all('article', class_='product_pod')
+        # Find all the links to books in the current category
+        book_links = soup.find_all('h3')
 
-        print(f"Category Page URL: {category_page_url}")
-        print("Book Information:")
+        for book_link in book_links:
+            relative_url = book_link.a['href']
+            book_url = urljoin(category_url, relative_url)
+            print("Book URL:", book_url)
 
-# Extracting from book title from the h3 tag within article
-        for book in book_containers:
-            book_title = book.h3.a['title']
+            scrape_book_details(book_url)
 
-            product_page_url = book.h3.a['href']
-
-            product_page_url = f'https://books.toscrape.com/catalogue{product_page_url}'
-            product_info = scrape_product_page(product_page_url)
-
-            # Print or process the extracted information as needed
-            print(f"Book Title: {book_title}")
-            print(f"Product Page URL: {product_page_url}")
-            print("Additional Information:")
-            print(product_info)
-            print("\n")
-
-def scrape_product_page(product_url):
-    response = requests.get(product_url)
+def scrape_book_details(book_url):
+    response = requests.get(book_url)
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Extract additional information from the product page
-        book_info = {
-            'book_title': soup.h1.text.strip(),
-            'product_description': soup.find('meta', {'name': 'description'})['content'].strip(),
-            'upc': soup.find('th', string='UPC').find_next('td').text.strip(),
-            'price_including_tax': soup.find('th', text='Price (incl. tax)').find_next('td').string.strip(),
-            'price_excluding_tax': soup.find('th', text='Price (excl. tax)').find_next('td').text.strip(),
-            'quantity_available': soup.find('th', text='Availability').find_next('td').text.strip(),
-            'category': soup.select_one('ul.breadcrumb > li:nth-child(3) > a').text.strip(),
-            # 'category': soup.find('a', {'href': '../../../category/books/business_35/index.html'}).text.strip(),
-            'review_rating': soup.find('th', text='Number of reviews').find_next('td').text.strip(),
-            'image_url': soup.find('div', {'class': 'item active'}).img['src'],
-        }
+        # Modify the following code to extract book information from the current book page
+        book_title = soup.h1.text.strip()
+        book_script = soup.find('div', class_='sub-header')
+        book_description = book_script.find_next('p').text.strip()
+        tbody_elements = soup.table.find_all('tr')
+        tbody_list = list(tbody_elements)
+        upc = tbody_list[0].td.text.strip()
+        price_w_tax = tbody_list[2].td.text.strip()
+        price_no_tax = tbody_list[3].td.text.strip()
+        avail = tbody_list[5].td.text.strip()
+        rating_tag = soup.find('p', class_="star-rating")
+        rating = rating_tag['class'][1]
+        img_url = urljoin(book_url, soup.img['src'])
 
-        return book_info
+        print(f"Book Title: {book_title}")
+        print(f"Book Description: {book_description}")
+        print(f"UPC: {upc}")
+        print(f"Price (incl. tax): {price_w_tax}")
+        print(f"Price (excl. tax): {price_no_tax}")
+        print(f"Availability: {avail}")
+        print(f"Star Rating: {rating}")
+        print(f"Image URL: {img_url}")
+        print("\n")
+
+# Example usage
+base_url = 'https://books.toscrape.com/index.html'
+scrape_site(base_url)
+
+
+# def scrape_site(base_url):
+#     response = requests.get(base_url)
+#
+#     if response.status_code == 200:
+#         soup = BeautifulSoup(response.text, 'html.parser')
+#
+#         articles = soup.find_all('article', class_='product_pod')
+#         for article in articles:
+#             book_title = article.h3.a['title']
+#             book_url = article.h3.a['href']
+#
+#             print(f'Title: {book_title}')
+#             print(f'URL: {base_url}/{book_url}\n')
+#
+# base_url = 'https://books.toscrape.com/index.html'
+# scrape_site(base_url)
+
+# ----------------
+
+# def scrape_catalog(catalog_url):
+#     response = requests.get(catalog_url)
+#
+#     if response.status_code == 200:
+#         soup = BeautifulSoup(response.text, 'html.parser')
+#
+#         category_list = soup.find_all('ul', class_="nav-list")
+#
+#         for category in category_list:
+#             # Find the anchor tag within the ul element
+#             anchor_tags = category.find_all('a')
+#
+#             for anchor_tag in anchor_tags:
+#                 relative_url = anchor_tag.get('href')
+#                 # Construct the full URL using urljoin
+#                 category_url = urljoin(catalog_url, relative_url)
+#                 print("Book Category URL:", category_url, "\n")
+#
+#                 scrape_books(category_url)
+#
+# def scrape_books(category_url):
+#     response = requests.get(category_url)
+#
+#     if response.status_code == 200:
+#         soup = BeautifulSoup(response.text, 'html.parser')
+#
+#         # Modify the following code to extract book information from the current category page
+#         book_titles = soup.find_all('h3')  # Adjust based on the actual structure
+#         for title in book_titles:
+#             print("Book Title:", title.text.strip())
+#
+#
+# catalog_url = 'https://books.toscrape.com/catalogue/category/books_1/index.html'
+# scrape_catalog(catalog_url)
+
+# -----------------------
+
+# def scrape_site(base_url):
+#     for i in range(1, 51):
+#         site_url = f'{base_url}/page-{i}.html'
+#         response = requests.get(site_url)
+#
+#         if response.status_code == 200:
+#             soup = BeautifulSoup(response.text, 'html.parser')
+#
+#             # nav_list = soup.find('ul', class_='nav-list')
+#             #
+#             # if nav_list:
+#             #     # Extract all category links from the navigation list
+#             #     category_links = nav_list.find_all('a')
+#             #
+#             #     # Extract category names from the links
+#             #     categories = [link.text.strip() for link in category_links]
+#             #
+#             #     # Print the list of categories
+#             #     # print(category_links)
+#             #     for category in categories:
+#             #         print(category)
+#
+#  # used to get all books of category
+#             article_containers = soup.find_all('ol')
+#             for article_container in article_containers:
+#                 articles = article_container.find_all('article', class_='product_pod')
+#
+#                 for article in articles:
+#                     category_name = article.select_one('div.image_container img')['alt']
+#
+#                     book_title = article.h3.a['title']
+#                     book_url = article.h3.a['href']
+#
+#                     print(f'Title: {book_title}')
+#                     print(f'URL: {book_url}\n')
+#                     # print(f'{category_name}\n')  # Print category for each book
+#
+# base_url = 'https://books.toscrape.com/catalogue'
+# scrape_site(base_url)
 
 
 
-# URL of current Product
-# product_url = 'https://books.toscrape.com/catalogue/the-dirty-little-secrets-of-getting-your-dream-job_994/index.html'
-category_url = 'https://books.toscrape.com/catalogue/category/books/business_35/index.html'
-# scrape_product_page(product_url)
-scrape_category_page(category_url)
+# _______________
 
 
+# def scrape_catalog_page(catalog_url):
+#     response = requests.get(catalog_url)
+#
+#     if response.status_code == 200:
+#         soup = BeautifulSoup(response.text, 'html.parser')
+#
+#         categories = []
+#         catalog_page_url = catalog_url
+#         category_containers = soup.find_all('ul', class_='nav-list')
+#
+#         for ul in category_containers:
+#             categories = ul.find_all('li')
+#
+#             for category in categories:
+#                 category_title = category.a.text.strip()
+#                 print(f"Category Title: {category_title}")
 
-# Site URL
-# site_url = 'https://books.toscrape.com/index.html'
+# class Book:
+#     def __init__(self, upc, product_title, price_including_tax, price_excluding_tax, quantity_available,
+#                  product_description, category, review_rating, image_url):
+#         self.upc = upc
+#         self.product_title = product_title
+#         self.price_including_tax = price_including_tax
+#         self.price_excluding_tax = price_excluding_tax
+#         self.quantity_available = quantity_available
+#         self.product_description = product_description
+#         self.category = category
+#         self.review_rating = review_rating
+#         self.image_url = image_url
+#
+# def scrape_category(category_url):
+#     response = requests.get(category_url)
+#
+#     if response.status_code == 200:
+#         soup = BeautifulSoup(response.text, 'html.parser')
+#
+#         category_containers = soup.find_all('article', class_='product_pod')
+#
+#         books_data = []
+#
+#         for container in category_containers:
+#             relative_product_url = container.a['href']
+#             full_product_url = urljoin(category_url, relative_product_url)
+#
+#             book_data = scrape_product_info(full_product_url)
+#             books_data.append(book_data)
+#
+#             # Writing all book info to csv
+#             csv_filename = 'category.csv'
+#             with open(csv_filename, 'w', newline='') as csvfile:
+#                 fieldnames = ['upc', 'book_title', 'price_including_tax',
+#                               'price_excluding_tax', 'quantity_available', 'product_description',
+#                               'category', 'review_rating', 'image_url']
+#                 writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+#                 writer.writeheader()
+#
+#                 for book in books_data:
+#                     writer.writerow({
+#                         'upc': book.upc,
+#                         'book_title': book.product_title,
+#                         'price_including_tax': book.price_including_tax,
+#                         'price_excluding_tax': book.price_excluding_tax,
+#                         'quantity_available': book.quantity_available,
+#                         'product_description': book.product_description,
+#                         'category': book.category,
+#                         'review_rating': book.review_rating,
+#                         'image_url': book.image_url
+#                     })
+#
+#             print("{csv_filename}")
+# def scrape_product_info(full_product_url):
+#     response = requests.get(full_product_url)
+#
+#     if response.status_code == 200:
+#         soup = BeautifulSoup(response.text, 'html.parser')
+#
+#         upc_th = soup.find('th', string='UPC')
+#         upc = upc_th.find_next('td').text.strip()
+#         product_title = soup.h1.text.strip()
+#         price_including_tax = soup.find('th', string='Price (incl. tax)').find_next('td').string.strip()
+#         price_excluding_tax = soup.find('th', string='Price (excl. tax)').find_next('td').text.strip()
+#         quantity_available = soup.find('th', string='Availability').find_next('td').text.strip()
+#         ul_element = soup.find('ul')
+#         list_items = ul_element.find_all('li')
+#         category = list_items[2].text.strip()
+#         review_rating = soup.find('th', string='Number of reviews').find_next('td').text.strip()
+#         image_url = soup.find('div', {'class': 'item active'}).img['src']
+#         product_description = soup.find('meta', {'name': 'description'})['content'].strip()
+#
+#         return Book(upc, product_title, price_including_tax, price_excluding_tax, quantity_available,
+#                     product_description, category, review_rating, image_url)
+# #
+# # # Example usage
+# #
+# # catalog_url = "https://books.toscrape.com/catalogue/category/books_1/index.html"
+# # scrape_catalog_page(catalog_url)
+# # # scrape_books_in_category(catalog_url)
+# category_url = 'https://books.toscrape.com/catalogue/category/books/fiction_10/index.html'
+# scrape_category(category_url)
 
 
